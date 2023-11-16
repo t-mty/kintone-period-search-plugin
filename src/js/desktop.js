f3cf505d369879cdba2ee5958d4a9e39a0d2e720
mty <73290752+t-mty@users.noreply.github.com>
2023-11-16 22:53:01 +0900

    Add files via upload

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -28,6 +28,7 @@ jQuery.noConflict();
 
     settingForm(spaceElement, config.fieldName); //フォームを設置（設置する場所, 左に表示するフィールド名）
     setValue($, config); //値をセット
+    
     //検索ボタンが押されたとき
     $('.search-button').click(function() {
       searchDate($, e, config) //検索
@@ -91,6 +92,7 @@ function setValue($, config) {
   }
   if(regexEnd.test(query)){
     endDate = query.match(regexEnd)[0].match(/"(.+)"/g)[0].replace(/"/g, '');
+    endDate = addDate(new Date(endDate), -1); //日付を-1日する
   }
 
   $('.start-date').val(startDate);
@@ -114,47 +116,46 @@ function getStringFromDate(d) {
 //検索
 function searchDate($, e, config) {
   const startDate = $('.start-date').val();
-
-  var date = new Date($('.end-date').val());
-  date.setDate(date.getDate() + 1);
-  var year = date.getFullYear().toString();
-  var mm = (date.getMonth()+1).toString();
-  var dd = date.getDate().toString();
-  var yyyymmdd = year + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
-  const endDate = yyyymmdd;
-
-  // var endDate = $('.end-date');
-  // var a = endDate.valueAsDate;
-  // endDate.valueAsDate = a.setDate(a.getDate()+1);
-  // const endDate = $('.end-date').val();
+  const endDate = addDate($('.end-date').val(), 1); //日付を1日プラスする
 
   var fieldCode = config.fieldCode; //対象のフィールドを宣言
-
-  // クエリ文字列生成
-  const createQueryString = (startDate, endDate, fieldCode) => {  
-    var query = kintone.app.getQueryCondition(); //現在のクエリ文字列を取得
-    var regexStart = new RegExp(`${fieldCode} >= "[0-9]{4}-[0-9]{2}-[0-9]{2}"`); //開始日の正規表現
-    var regexEnd = new RegExp(`${fieldCode} < "[0-9]{4}-[0-9]{2}-[0-9]{2}"`); //終了日の正規表現
-
-    if (query === ''){ //現在クエリが未指定の時
-      query = fieldCode +' >= "' + startDate + '" and ' + fieldCode +' < "'+ endDate + '"'; //開始日と終了日を設定する
-    }else if(regexStart.test(query) && regexEnd.test(query)){
-      query = query.replace(regexStart, fieldCode + '>= "' + startDate + '"'); // 開始日を変更
-      query = query.replace(regexEnd, fieldCode + '< "' + endDate + '"'); // 終了日を変更
-    }else if(regexStart.test(query)) {
-      query = query.replace(regexStart, fieldCode + '>= "' + startDate + '"'); // 開始日を変更
-      query = query + ' and ' + fieldCode +' < "'+ endDate + '"'; //終了日を追加
-    }else if(regexEnd.test(query)){
-      query = query + ' and ' + fieldCode +' >= "' + startDate + '"'; //開始日を追加
-      query = query.replace(regexEnd, fieldCode + '< "' + endDate + '"'); // 終了日を変更
-    }else{
-      query = query + ' and ' + fieldCode +' >= "' + startDate + '" and ' + fieldCode +' < "'+ endDate + '"'; //既存のクエリに開始日と終了日を追加する
-    }
-    return '&query=' + query;
-  };
 
   //URLを作成
   const viewpath = '?view=' + e.viewId;
   const query = createQueryString(startDate, endDate, fieldCode); //引数として開始日、終了日、フィールドコードを渡す
   document.location = location.origin + location.pathname + viewpath + encodeURI(query);
+}
+
+// クエリ文字列生成
+const createQueryString = (startDate, endDate, fieldCode) => {  
+  var query = kintone.app.getQueryCondition(); //現在のクエリ文字列を取得
+  var regexStart = new RegExp(`${fieldCode} >= "[0-9]{4}-[0-9]{2}-[0-9]{2}"`); //開始日の正規表現
+  var regexEnd = new RegExp(`${fieldCode} < "[0-9]{4}-[0-9]{2}-[0-9]{2}"`); //終了日の正規表現
+
+  if (query === ''){ //現在クエリが未指定の時
+    query = fieldCode +' >= "' + startDate + '" and ' + fieldCode +' < "'+ endDate + '"'; //開始日と終了日を設定する
+  }else if(regexStart.test(query) && regexEnd.test(query)){
+    query = query.replace(regexStart, fieldCode + '>= "' + startDate + '"'); // 開始日を変更
+    query = query.replace(regexEnd, fieldCode + '< "' + endDate + '"'); // 終了日を変更
+  }else if(regexStart.test(query)) {
+    query = query.replace(regexStart, fieldCode + '>= "' + startDate + '"'); // 開始日を変更
+    query = query + ' and ' + fieldCode +' < "'+ endDate + '"'; //終了日を追加
+  }else if(regexEnd.test(query)){
+    query = query + ' and ' + fieldCode +' >= "' + startDate + '"'; //開始日を追加
+    query = query.replace(regexEnd, fieldCode + '< "' + endDate + '"'); // 終了日を変更
+  }else{
+    query = query + ' and ' + fieldCode +' >= "' + startDate + '" and ' + fieldCode +' < "'+ endDate + '"'; //既存のクエリに開始日と終了日を追加する
+  }
+  return '&query=' + query;
+};
+
+//日付データにn日加算する
+function addDate(dateValue, n) {
+  var date = new Date(dateValue);
+  date.setDate(date.getDate() + n);
+  var year = date.getFullYear().toString();
+  var mm = (date.getMonth()+1).toString();
+  var dd = date.getDate().toString();
+  var yyyymmdd = year + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+  return yyyymmdd;
 }
